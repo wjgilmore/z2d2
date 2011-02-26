01ed0682d00f4ecf3ee5a4912233ccf78ede6b74
Jason Gilmore <wj@wjgilmore.com>
2011-02-25 22:36:55 -0500

    Removed APPLICATION_ENV setting in the test bootstrap since easier to define in the controller and model test helper classes

--- tests/application/bootstrap.php
+++ tests/application/bootstrap.php
@@ -4,8 +4,6 @@ define('BASE_PATH', realpath(dirname(__FILE__) . '/../../'));
 
 define('APPLICATION_PATH', BASE_PATH . '/application');
 
-// Include path
-
 set_include_path(    
   '.'    
    . PATH_SEPARATOR . BASE_PATH . '/library'    
@@ -17,8 +15,6 @@ set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../application/models'),
     get_include_path(),
 )));
-
-define('APPLICATION_ENV', 'testing');
 
 require_once 'controllers/ControllerTestCase.php';
 require_once 'models/ModelTestCase.php';
