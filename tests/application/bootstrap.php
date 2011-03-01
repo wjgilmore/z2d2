5ffc36207602d0d06167b989c3b39f8f97ce998c
Jason Gilmore <wj@wjgilmore.com>
2011-02-28 22:29:51 -0500

    Enabled test log generation in phpunit.xml

--- tests/application/bootstrap.php
+++ tests/application/bootstrap.php
@@ -4,18 +4,6 @@ define('BASE_PATH', realpath(dirname(__FILE__) . '/../../'));
 
 define('APPLICATION_PATH', BASE_PATH . '/application');
 
-set_include_path(    
-  '.'    
-   . PATH_SEPARATOR . BASE_PATH . '/library'    
-   . PATH_SEPARATOR . get_include_path()
-);
-
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../application/views/scripts'),
-    realpath(APPLICATION_PATH . '/../application/models'),
-    get_include_path(),
-)));
-
 require_once 'controllers/ControllerTestCase.php';
 require_once 'models/ModelTestCase.php';
 
